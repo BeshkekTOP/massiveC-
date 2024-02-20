dbd59eb80719f1c0850e60bd84daf7169b5f5cfd
KUZYA_LAKOMKIN <136304665+BeshkekTOP@users.noreply.github.com>
2024-02-20 10:16:03 +0300

    Add files via upload

---
+++ massiv.c++.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    setlocale(0, "");
+
+    double A[3][5];
+    for (int i = 0; i < 3; ++i)
+        for (int j = 0; j < 5; ++j)
+            cin >> A[i][j];
+
+    for (int i = 0; i < 3; i++) {
+        double sum = 0;
+        for (int j = 0; j < 5; j++) {
+            sum += A[i][j];
+        }
+        double average = sum / 5;
+        cout << "Среднее арифметическое для " << i + 1 << " строки: " << average << endl;
+    }
+
+    return 0;
+}
+
+
+
